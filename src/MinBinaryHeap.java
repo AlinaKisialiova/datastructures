4d373d6132445eaf188677a53327c157d68f7f50
Alina Kisialiova <alina.kisialiova@ericsson.com>
2020-02-07 16:49:12 +0000

    Add Binary Heap description.

--- src/MinBinaryHeap.java
+++ src/MinBinaryHeap.java
@@ -1,5 +1,21 @@
 import java.util.NoSuchElementException;
 
+/**
+ * A binary heap is defined as a binary tree with two additional constraints:
+ *
+ * Shape property: a binary heap is a complete binary tree; that is, all levels of the tree, except possibly the last one (deepest) are fully filled, and,
+ * if the last level of the tree is not complete, the nodes of that level are filled from left to right.
+ *
+ * Heap property: the key stored in each node is either greater than or equal to (≥) or less than or equal to (≤) the keys in the node's children, according to some total order.
+ *
+ * Examples of Min Heap:
+ *
+ *             10                      10
+ *          /      \               /       \
+ *        20        100          15         30
+ *       /                      /  \        /  \
+ *     30                     40    50    100   40
+ */
 public class MinBinaryHeap {
 
     public static void main(String[] args) {
